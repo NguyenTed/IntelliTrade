465db23e28d175108916dd6afa2baf3dc8279140
Van Ky <kydang26092004@gmail.com>
2025-08-29 22:46:25 +0700

    update deploy-backtest

--- backend/backtest-service/run.py
+++ backend/backtest-service/run.py
@@ -5,5 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.getenv("PORT", "5005"))
     app.run(host="0.0.0.0", port=port, debug=False)  # <- quan trọng: 0.0.0.0
