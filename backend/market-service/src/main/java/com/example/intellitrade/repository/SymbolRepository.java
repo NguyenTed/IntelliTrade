c3f3ea8880e89e3367e9a5c58bab72d591b2603b
Van Ky <kydang26092004@gmail.com>
2025-08-29 09:18:03 +0700

    fix and deploy backtest

--- backend/market-service/src/main/java/com/example/intellitrade/repository/SymbolRepository.java
+++ backend/market-service/src/main/java/com/example/intellitrade/repository/SymbolRepository.java
@@ -1,7 +1,7 @@
-package com.example.intellitrade.repository;
-
-import com.example.intellitrade.model.Symbol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SymbolRepository extends MongoRepository<Symbol, String> {
-}
+//package com.example.intellitrade.repository;
+//
+//import com.example.intellitrade.model.Symbol;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface SymbolRepository extends MongoRepository<Symbol, String> {
+//}
